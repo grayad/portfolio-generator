fdbe8de3451bfb280fec62f40c704e9488e3d84b
Alexus Gray <alexus_gray@alumni.unc.edu>
2022-06-13 18:19:02 -0400

    write to file

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const inquirer= require('inquirer');
+const fs = require('fs');
+
+// assign HTML template function in page-template.js to variable generate page
+const generatePage = require('./src/page-template.js');
+
 const promptUser = () => {
     return inquirer.prompt([
         {
@@ -125,18 +130,17 @@ const promptProject = portfolioData => {
         }
     });
 };
-  
+
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html',pageHTML, err => {
+        if (err) throw new Error(err);
+
+        console.log('Portfolio complete! Check out index.html to see the output!');
+    });
 });
-// const generatePage = require('./src/page-template.js');
 
-// const pageHTML = generatePage(name, github);
 
-// fs.writeFile('./index.html', generatePage(name, github), err => {
-//     if (err) throw new Error(err);
-
-//     console.log('Portfolio complete! Check out index.html to see the output!');
-// });
